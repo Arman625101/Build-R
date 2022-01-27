e720b76cdc4066ef2706d487c3affd2c8cdb06c9
Arman625101 <vip.arman2011@gmail.com>
2022-01-28 01:27:45 +0400

    created minimal <parts> section

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,41 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'none',
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-      watch: true
-    }
-  },
-  plugins: [new HtmlWebpackPlugin()]
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+            watch: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.(s[ac]ss$)|(css$))/i,
+                use: ['css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'assets/[name].[ext]'
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ]
 };
